2d30ea4fdc62bc5a1a613874ae72136ca1a79ffb
Kate Schlunz <schlunzk@gmail.com>
2017-11-19 17:00:17 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -34,9 +34,9 @@ class Question{
     return new Promise(function(resolve){
       db.get(sql, [id], function(err, result){
         const question= new Question()
-        question.id = result.id 
+        question.id = result.id
         resolve(question)
-      }
+      })
 
     })
   }
